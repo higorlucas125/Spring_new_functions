958b20f4f34ec7d1e654fb7ac9b41d7fdecce4f3
higorlucas125 <higorlaf@hotmail.com>
2025-03-17 23:25:17 -0300

    Iniciando novas conhecimento no spring boot
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/abc/cursos/desenvolvimento/DesenvolvimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.cursos.desenvolvimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
